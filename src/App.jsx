48c58c5c76b47da3c0fc65c28f86f105b47bd02d
sandula-sanchana <sandulasanchana@gmail.com>
2025-12-25 22:42:58 +0530

    first commit +spline

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {Navbar} from "./components/Navbar.jsx";
+
+function App() {
+
+
+  return (
+    <>
+        <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Spline from '@splinetool/react-spline';
+
+export const Navbar=()=>{
+
+
+    return (
+        <>
+            <main style={{ width: '100%', height: '100vh' }}>
+                <Spline
+                    scene="https://prod.spline.design/LtW1ZRVgxJNmnIh9/scene.splinecode"
+                />
+            </main>
+        </>
+    )
+}
