97de169dd8b40b022e146640ae6efb02de44e17b
sandula-sanchana <sandulasanchana@gmail.com>
2026-01-01 13:59:43 +0530

    add route to contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {Routes,Route} from 'react-router-dom'
 import {Home} from "./pages/Home.jsx";
 import {Workpage} from "./pages/Workpage.jsx";
 import SkillsPage from "./pages/Skillspage.jsx";
+import {ContactSection} from "./pages/ContactSection.jsx";
 
 function App() {
 
@@ -16,6 +17,7 @@ function App() {
               <Route path='/' element={<Home/>}/>
               <Route path='/work' element={<Workpage/>}/>
               <Route path='/skills' element={<SkillsPage/>}/>
+              <Route path='/contact' element={<ContactSection/>}/>
           </Routes>
     </>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import {Hero3D} from "../components/Hero3D.jsx";
 import {Navbar} from "../components/Navbar.jsx";
 import {AboutSection} from "../components/AboutSection.jsx";
 import {ContactSection} from "./ContactSection.jsx";
+import Skillspage from "./Skillspage.jsx";
 
 
 export const Home=()=>{
